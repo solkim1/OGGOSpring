af628a8dff9979946d5b2b73ecffb32f3573ea22
김솔 <sr139@naver.com>
2024-08-24 14:39:02 +0900

    구글캘린더 합쳤음

--- src/main/java/com/oggo/planmaker/model/User.java
+++ src/main/java/com/oggo/planmaker/model/User.java
@@ -11,6 +11,30 @@ public class User {
 
 	private String userId;
 
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
 	private String userPw;
 
 	private String userEmail;
