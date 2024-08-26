a01205befe962087a55cf175f013bb5abe8477aa
Jo <jominseok613@gmail.com>
2024-08-26 10:15:36 +0900

    08-26 10:14 Pull

--- src/main/java/com/oggo/planmaker/model/User.java
+++ src/main/java/com/oggo/planmaker/model/User.java
@@ -14,5 +14,34 @@ public class User {
 	private String userPw;
 
 	private String userEmail;
+	
+	public String getUserId() {
+		return userId;
+	}
 
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userPw=" + userPw + ", userEmail=" + userEmail + "]";
+	}
 }
